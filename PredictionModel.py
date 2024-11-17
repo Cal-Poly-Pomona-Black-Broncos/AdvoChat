93936a8590a8174c23d8ed248d7da8ec107bf02d
Britney <166865959+Bcollier05@users.noreply.github.com>
2024-11-16 22:24:22 -0800

    PredictionModel.py

--- PredictionModel.py
+++ PredictionModel.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import sklearn 
 import numpy as np
+import json
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,10 +10,13 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 
-#Data Cleaning & Preprocessing
+#Import Data
 medData = pd.read_csv('C:/Users/Britn/OneDrive - Cal Poly Pomona/FRC Hackathon/healthcare_dataset.csv', encoding = 'ISO-8859-1')
+
+#Removes empty rows/columns
 medData.dropna(how='all')
 
+#Data encoding
 label_encoder = LabelEncoder()
 medData['Gender'] = label_encoder.fit_transform(medData['Gender' ])
 medData['Medical Condition'] = label_encoder.fit_transform(medData['Medical Condition' ])
@@ -20,7 +24,7 @@ medData['Insurance Provider'] = label_encoder.fit_transform(medData['Insurance P
 medData['Admission Type'] = label_encoder.fit_transform(medData['Admission Type' ])
 
 
-
+#Preprocessing & Cleaning
 x = medData.iloc[:,[1,2,3,5,6,7,11]]
 y=medData.iloc[:, [12]]
 
@@ -35,8 +39,8 @@ y_clean = y[:min_length]
 
 #Data Split
 x_train, x_test, y_train, y_test = train_test_split(x_clean, y_clean, test_size = 0.2, random_state = 42)
-x_train = x_train
-y_train = y_train
+y_train = y_train.values.ravel()
+y_test = y_test.values.ravel()
 
 
 #Random Forest Implementation
@@ -45,8 +49,15 @@ rf.fit(x_train, y_train)
 
 y_pred = rf.predict(x_test)
 
-
+#Results
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Random Forest Classifier Accuracy: {accuracy*100:.2f}")
-print(classification_report(y_test, y_pred))
+print(f"Random Forest Classifier Accuracy: {accuracy*100:.4f}")
+report = classification_report(y_test, y_pred)
+
+#Export Json File
+report_json = json.dumps(report, indent=4)
+print(report_json)
+
+
+
 
