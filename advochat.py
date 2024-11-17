449c8b8551fc26c1033447d1f3f5709010310435
gad.maj <gadymajor@gmail.com>
2024-11-16 20:30:56 -0800

    sheetreader added

--- advochat.py
+++ advochat.py
@@ -2,6 +2,7 @@ import os
 from openai import OpenAI
 
 #API SETUP + CONVO HEADER & CONVO ARRAY
+API_KEY = "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" 
 client = OpenAI(api_key=API_KEY)
 
 conversation_history = [{"role": "system", "content": "You are a medical assistant. I will provide you with a json file with a patients background information, and further details to help aid the paitent, you will only contextualize"}]
@@ -21,7 +22,7 @@ def chat_with_gpt(user_input):
     
     return assistant_reply
 
-#CHAT LOOP
+#CHAT LOOP --> this should be looped on the front end?
 if __name__ == "__main__":
     print("Chat with GPT (type 'exit' to quit):")
     while True:
---
+++ sheetreader.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import csv
+import json
+
+response = requests.get("https://docs.google.com/spreadsheets/d/1Mt5Ep5oEZB3_Qj3sjaAaL5jmi6L3iqhcQx14keK6prI/gviz/tq?tqx=out:csv")
+
+json_form = os.path.join("data", "questionnaire_form.json")
+
+
+#helper method to save json with data into ./data and with name
+#input data -> raw json data
+#input name -> name of json file being saved
+def save_json(data, name):
+    with open(os.path.join("data", name), "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, indent=4)
+
+
+#download form into json
+def csv_json():
+    if response.status_code == 200:
+        csv_content = response.content.decode("utf-8")
+        csv_reader = csv.DictReader(csv_content.splitlines())
+        json_response = [row for row in csv_reader]
+
+        save_json(json_response, "questionnaire_form.json")
+    else:
+        print(f"failed with code: {response.status_code} lol")
+
+
+#parse json to only response with email
+def parse_json(email):
+    with open(json_form, "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        filtered_data = [row for row in data if row.get("Email ", "").strip().lower() == email.strip().lower()]
+        
+        #grab latest repsonse
+        if filtered_data:
+            latest_response = max(filtered_data, key=lambda x: x.get("Timestamp"))
+            save_json([latest_response], "individual_form.json")
+
+csv_json()
+parse_json("brurh@gmail.com")
