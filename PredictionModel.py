b202f012cd739ae26c178df460f2ab640cda2b1f
Britney <166865959+Bcollier05@users.noreply.github.com>
2024-11-16 23:17:03 -0800

    PredictionModel.py

--- PredictionModel.py
+++ PredictionModel.py
@@ -10,13 +10,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 
-#Import Data
+#Data Cleaning & Preprocessing
 medData = pd.read_csv('C:/Users/Britn/OneDrive - Cal Poly Pomona/FRC Hackathon/healthcare_dataset.csv', encoding = 'ISO-8859-1')
-
-#Removes empty rows/columns
 medData.dropna(how='all')
 
-#Data encoding
 label_encoder = LabelEncoder()
 medData['Gender'] = label_encoder.fit_transform(medData['Gender' ])
 medData['Medical Condition'] = label_encoder.fit_transform(medData['Medical Condition' ])
@@ -24,7 +21,7 @@ medData['Insurance Provider'] = label_encoder.fit_transform(medData['Insurance P
 medData['Admission Type'] = label_encoder.fit_transform(medData['Admission Type' ])
 
 
-#Preprocessing & Cleaning
+#Data Split
 x = medData.iloc[:,[1,2,3,5,6,7,11]]
 y=medData.iloc[:, [12]]
 
@@ -37,27 +34,58 @@ min_length = min(len(x), len(y))
 x_clean = x[:min_length]
 y_clean = y[:min_length]
 
-#Data Split
+
 x_train, x_test, y_train, y_test = train_test_split(x_clean, y_clean, test_size = 0.2, random_state = 42)
 y_train = y_train.values.ravel()
 y_test = y_test.values.ravel()
 
 
-#Random Forest Implementation
+#Random Forest Model
 rf = RandomForestClassifier(class_weight='balanced')
 rf.fit(x_train, y_train)
 
 y_pred = rf.predict(x_test)
 
-#Results
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Random Forest Classifier Accuracy: {accuracy*100:.4f}")
 report = classification_report(y_test, y_pred)
 
-#Export Json File
 report_json = json.dumps(report, indent=4)
-print(report_json)
+
+# dictionary to store encoders for categorical columns
+encoders = {}
+
+# =categorical columns
+categorical_columns = ['Gender', 'Medical Condition', 'Insurance Provider', 'Admission Type']
+
+# LabelEncoders for each categorical column
+for col in categorical_columns:
+    encoder = LabelEncoder()
+    medData[col] = encoder.fit_transform(medData[col])
+    encoders[col] = encoder
+
+def handle_unseen_category(encoder, value):
+    if value in encoder.classes_:
+        return encoder.transform([value])[0]  # Transform and return the encoded value
+    else:
+        return -1
 
 
+def predict_hospital(model, x_clean, user_input, encoders):
+    input_df = pd.DataFrame([user_input], columns=x_clean.columns)
 
+    
+    for col in input_df.columns:
+        if col in encoders:  
+            encoder = encoders[col]
+            value = input_df[col].iloc[0]  
+            encoded_value = handle_unseen_category(encoder, value)
+            input_df[col] = encoded_value
+    prediction = model.predict(input_df)
+    return prediction
+
+#Test
+user_input = [20, 'Male', 'Cancer', 'Medicare', 18856.28131, 'Urgent', 10 ]
+
+print(predict_hospital(rf, x_clean, user_input, encoders))
 
